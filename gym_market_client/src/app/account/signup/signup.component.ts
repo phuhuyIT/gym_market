91141967d74a955a885319902503d903ca70c9a0
VinhTin-AQUA <tinhovinh@gmail.com>
2024-09-03 23:22:52 +0700

    update form

--- gym_market_client/src/app/account/signup/signup.component.ts
+++ gym_market_client/src/app/account/signup/signup.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
-  standalone: true,
-  imports: [HeaderComponent],
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+	selector: 'app-signup',
+	standalone: true,
+	imports: [HeaderComponent, ReactiveFormsModule],
+	templateUrl: './signup.component.html',
+	styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+	signUpForm!: FormGroup;
+	submit = false;
 
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit() {
+		this.signUpForm = this.formBuilder.group({
+			fullName: ['', [Validators.required]],
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', [Validators.required]],
+			confirmPassword: ['', [Validators.required]],
+			role: ['PT'],
+		});
+	}
+
+	// đánh dấu các trường không hợp lệ và hiển thị lỗi tương ứng
+	addDirty(control: string) {
+		this.signUpForm.controls[control].markAsDirty({ onlySelf: true });
+	}
+
+	removeDirty(control: string) {
+		this.signUpForm.controls[control].markAsPristine({
+			onlySelf: true,
+		});
+	}
+
+	onSignUp() {
+		this.submit = true;
+		if (this.signUpForm.valid == false) {
+			return;
+		}
+
+		if (
+			this.signUpForm.controls['password'].value !==
+			this.signUpForm.controls['confirmPassword'].value
+		) {
+			this.addDirty('confirmPassword');
+			return;
+		} else {
+		}
+
+		console.log(this.signUpForm.value);
+	}
 }
