c074b6b3f22b8f3d8e9919463ed620e0417fd452
VinhTin-AQUA <tinhovinh@gmail.com>
2024-09-03 23:22:52 +0700

    update form

--- gym_market_client/src/app/account/login/login.component.ts
+++ gym_market_client/src/app/account/login/login.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  signUpForm!: FormGroup
+  submit = false;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['',[Validators.required]]
+    })
+  }
+
+  // đánh dấu các trường không hợp lệ và hiển thị lỗi tương ứng
+  validateAllFormFields(control: string) {
+    this.signUpForm.controls[control].markAsDirty({ onlySelf: true })
+  }
+
+  onSignUp() {
+    this.submit = true;
+    if(this.signUpForm.valid == false) {
+
+      return;
+    }
+
+    console.log(this.signUpForm.value)
+  }
 }
