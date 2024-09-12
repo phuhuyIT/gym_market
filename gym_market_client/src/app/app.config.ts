8709201cd54b788010ddf9c2cdef0745c1ba691f
VinhTin-AQUA <tinhovinh@gmail.com>
2024-09-12 11:25:34 +0700

    add my-course page

--- gym_market_client/src/app/app.config.ts
+++ gym_market_client/src/app/app.config.ts
@@ -1,9 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+	provideRouter,
+	withComponentInputBinding,
+	withInMemoryScrolling,
+	withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+	providers: [
+		provideRouter(
+			routes,
+			withViewTransitions(),
+			withComponentInputBinding(),
+			withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' })
+		),
+		provideHttpClient(),
+	],
 };
--- gym_market_client/src/app/pages/courses/course.routes.ts
+++ gym_market_client/src/app/pages/courses/course.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
+import { MyCoursesComponent } from './my-courses/my-courses.component';
 
 export const routes: Routes = [
 	{
@@ -11,6 +12,11 @@ export const routes: Routes = [
 				path: 'course-details',
 				component: CourseDetailsComponent,
 				title: 'Chi tiết khóa học',
+			},
+			{
+				path: 'my-courses/:type',
+				component: MyCoursesComponent,
+				title: 'Khóa học của bạn',
 			},
 		],
 	},
