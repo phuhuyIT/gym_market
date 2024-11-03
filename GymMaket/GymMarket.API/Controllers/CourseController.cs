404987388625f704be86c1299ed258c17b279807
phuhuykoxl <phuhuy02003@gmail.com>
2024-11-03 22:34:20 +0700

    Create GenericController and modify Controllers to suitable with it

---
+++ GymMaket/GymMarket.API/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using GymMarket.API.Models;
+using GymMarket.API.Repositories.IRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMarket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : GenericController<Course, string>
+    {
+        public CourseController(IGenericRepository<Course, string> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+
+        protected override string GetEntityId(Course entity)
+        {
+            return entity.CourseId;
+        }
+    }
+}
---
+++ GymMaket/GymMarket.API/Controllers/GenericController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using GymMarket.API.Models;
+using GymMarket.API.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMarket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class GenericController<TEntity, TKey> : ControllerBase where TEntity : class
+    {
+        protected readonly IGenericRepository<TEntity, TKey> _repository;
+        protected readonly IMapper _mapper;
+
+        protected GenericController(IGenericRepository<TEntity, TKey> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET: api/[controller]
+        [HttpGet]
+        public virtual async Task<IActionResult> GetAll()
+        {
+            var entities = await _repository.GetAllAsync();
+            return Ok(entities);
+        }
+
+        // GET: api/[controller]/{id}
+        [HttpGet("{id}")]
+        public virtual async Task<IActionResult> GetById(TKey id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity == null)
+                return NotFound();
+            return Ok(entity);
+        }
+
+        // POST: api/[controller]
+        [HttpPost]
+        public virtual async Task<IActionResult> Create<TCreateDto>([FromBody] TCreateDto createDto) where TCreateDto : class
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var entity = _mapper.Map<TCreateDto, TEntity>(createDto);
+            await _repository.AddAsync(entity);
+            return CreatedAtAction(nameof(GetById), new { id = GetEntityId(entity) }, entity);
+        }
+
+        // PUT: api/[controller]/{id}
+        [HttpPut("{id}")]
+        public virtual IActionResult Update<TUpdateDto>([FromBody] TUpdateDto updateDto) where TUpdateDto : class
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var entity = _mapper.Map<TUpdateDto, TEntity>(updateDto);
+            _repository.Update(entity);
+            return NoContent();
+        }
+
+        // DELETE: api/[controller]/{id}
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete(TKey id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            if (entity == null)
+                return NotFound();
+
+            _repository.Remove(entity);
+            return NoContent();
+        }
+
+        // Helper method to get entity ID - should be implemented by derived classes
+        protected abstract TKey GetEntityId(TEntity entity);
+    }
+
+}
