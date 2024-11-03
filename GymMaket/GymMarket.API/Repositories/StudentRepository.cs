1c9568bf0663a320dd5736bc2dd55088ca2999be
phuhuykoxl <phuhuy02003@gmail.com>
2024-11-03 22:35:09 +0700

    Add repository

---
+++ GymMaket/GymMarket.API/Repositories/StudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using GymMarket.API.Data;
+using GymMarket.API.Models;
+
+namespace GymMarket.API.Repositories
+{
+    public class StudentRepository : GenericRepository<Student, string>
+    {
+        public StudentRepository(GymMarketContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
