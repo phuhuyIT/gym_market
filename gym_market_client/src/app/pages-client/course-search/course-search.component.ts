08767853d904a9818b1e9eca8793837fd5c1d04e
VinhTin-AQUA <tinhovinh@gmail.com>
2024-12-22 00:42:24 +0700

    update code

--- gym_market_client/src/app/pages-client/course-search/course-search.component.ts
+++ gym_market_client/src/app/pages-client/course-search/course-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CourseAgencyService } from '../../page-agency/course-agency.service';
 import { LoaderModalStore } from '../../stores/loader.store';
 import { patchState } from '@ngrx/signals';
@@ -14,16 +14,26 @@ import { patchState } from '@ngrx/signals';
 export class CourseSearchComponent {
 	courses: any;
 	loader = inject(LoaderModalStore);
+	pageIndex: number = 1;
+	pageSize: number = 10;
 
-	constructor(private courseService: CourseAgencyService) {}
+	constructor(
+		private courseService: CourseAgencyService,
+		private activatedRoute: ActivatedRoute,
+        private router: Router
+	) {}
 
 	ngOnInit() {
-		this.getCourses();
+		this.activatedRoute.queryParams.subscribe((res: any) => {
+			this.pageIndex = res.pageIndex || 1;
+			this.pageSize = res.pageSize || 10;
+			this.getCourses();
+		});
 	}
 
 	private getCourses() {
 		patchState(this.loader, { isShow: true });
-		this.courseService.getCourses().subscribe({
+		this.courseService.getCourses(this.pageIndex, this.pageSize).subscribe({
 			next: (res: any) => {
 				// console.log(res);
 				this.courses = res;
@@ -34,5 +44,18 @@ export class CourseSearchComponent {
 
 	onSubmit() {
 		console.log(123);
+	}
+
+	nexPage() {
+		this.pageIndex++;
+		this.router.navigate(['/client/course-search'], {queryParams: {pageIndex: this.pageIndex, pageSize: this.pageSize}});
+	}
+
+	prevPage() {
+		this.pageIndex--;
+		if (this.pageIndex < 1) {
+			this.pageIndex = 1;
+		}
+		this.router.navigate(['/client/course-search'], {queryParams: {pageIndex: this.pageIndex, pageSize: this.pageSize}});
 	}
 }
